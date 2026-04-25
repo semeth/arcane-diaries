1708914c19d693583e89b53c09c16d89dd2230c3
Andrei <thesemeth@gmail.com>
2026-04-25 03:04:43 +0300

    removed hardcoded files and added moddatamapprovider

---
+++ src/main/java/net/semeth/arcanediaries/datagen/ModDataMapProvider.java
@@ -0,0 +1,23 @@
+package net.semeth.arcanediaries.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.FurnaceFuel;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+import net.semeth.arcanediaries.item.ModItems;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModDataMapProvider extends DataMapProvider {
+    protected ModDataMapProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(packOutput, lookupProvider);
+    }
+
+    @Override
+    protected void gather(HolderLookup.Provider provider) {
+        this.builder(NeoForgeDataMaps.FURNACE_FUELS)
+                .add(ModItems.FROSTFIRE_ICE.getId(), new FurnaceFuel(1200),false)
+                .add(ModItems.STARLIGHT_ASHES.getId(), new FurnaceFuel(2400), false);
+    }
+}
