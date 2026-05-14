17e48b49fd676ab200c5f0f926569ecf305dee88
Andrei <thesemeth@gmail.com>
2026-05-15 00:31:28 +0300

    learned events with end rod to sheep example

--- src/main/java/net/semeth/arcanediaries/event/ModEvents.java
+++ src/main/java/net/semeth/arcanediaries/event/ModEvents.java
@@ -1,11 +1,17 @@
 package net.semeth.arcanediaries.event;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.semeth.arcanediaries.ArcaneDiaries;
 import net.semeth.arcanediaries.item.custom.HammerItem;
@@ -37,6 +43,17 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void livingDamage(LivingDamageEvent.Pre event){
+        if(event.getEntity() instanceof  Sheep sheep && event.getSource().getDirectEntity() instanceof Player player){
+            if(player.getMainHandItem().getItem() == Items.END_ROD){
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " just hit a sheep with End Rod"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 6));
+                player.getMainHandItem().shrink(1);
             }
         }
     }
